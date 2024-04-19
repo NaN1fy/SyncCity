d75ff3c11cc58092539441de6f3bb7ccebefed81
Guglielmo <Barison>
2024-04-19 13:39:58 +0200

    revert

--- PyMockSensors/src/stream_writer/stdout_stream_writer.py
+++
@@ -1,6 +0,0 @@
-from src.stream_writer.stream_writer_interface import StreamWriterInterface
-
-class StdoutStreamWriter(StreamWriterInterface):
-    def write(self, data: str) -> None:
-        print(data + '\n')
-
--- PyMockSensors/src/toolkit/__init__.py
+++
@@ -1,4 +0,0 @@
-import src.toolkit.coordinates
-import src.toolkit.sensor_type
-import src.toolkit.constants
-import src.toolkit.jsonfy
