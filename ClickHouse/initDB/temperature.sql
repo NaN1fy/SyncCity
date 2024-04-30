270dcee7c469751929a82a3be985b813544b998c
hash-Guglio <guglielmobarison00@gmail.com>
2024-04-30 15:25:51 +0200

    general fix

--- ClickHouse/initDB/temperature.sql
+++ ClickHouse/initDB/temperature.sql
@@ -1,4 +1,4 @@
-CREATE TABLE sc_database.raw_temperature
+CREATE TABLE sc_database.topic_temperature
 (
     rawJSON String
 ) ENGINE = Kafka('kafka:9092', 'temperature', 'SyncCity', 'JSONAsString');
@@ -8,19 +8,20 @@ CREATE TABLE sc_database.temperature
     sensor_name String,
     sensor_id String,
     gather_time DATETIME64,
-    latitude FLOAT,
-    longitude FLOAT,
-    readings FLOAT
+    latitude Float64,
+    longitude Float64,
+    readings Float64
 ) ENGINE = MergeTree()
-ORDER BY (sensor_id, gather_time);
+    ORDER BY (sensor_id, gather_time);
 
 CREATE MATERIALIZED VIEW sc_database.temperature_mv
 TO sc_database.temperature
 AS
 SELECT
     JSONExtractString(rawJSON, 'sensor_name') AS sensor_name,
-    toDateTime64(JSONExtractString(rawJSON, 'gather_time'), 1) AS gather_time,
-    JSONExtractFloat(rawJSON, 'coordinates', 1) AS latitude,
-    JSONExtractFloat(rawJSON, 'coordinates', 2) AS longitude,
+    JSONExtractString(rawJSON, 'sensor_id') AS sensor_id,
+    toDateTime64(JSONExtractString(rawJSON, 'gather_time'), 0) AS gather_time,
+    JSONExtractFloat(rawJSON, 'coordinates', 'coordinates', 1) AS latitude,
+    JSONExtractFloat(rawJSON, 'coordinates', 'coordinates', 2) AS longitude,
     JSONExtractFloat(rawJSON, 'readings', 1, 'value') AS readings
-FROM sc_database.raw_temperature;
+FROM sc_database.topic_temperature;
