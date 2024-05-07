7a78c6b2bfd7e829a7af1501b64d55ba97133345
Guglielmo Barison <guglielmobarison00@gmail.com>
2024-05-07 15:34:19 +0200

    3_sprint

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Scarica il plugin ClickHouse
-grafana-cli --pluginsDir "/var/lib/grafana/plugins" plugins install vertamedia-clickhouse-datasource
-
-# Riavvia il servizio Grafana
-docker-compose restart grafana
