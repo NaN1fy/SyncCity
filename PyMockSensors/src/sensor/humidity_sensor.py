ab87163f954b43fe24756bd9bbcf8f7a73b7d7ac
Davide <davide.dona02@gmail.com>
2024-05-08 21:54:23 +0200

    fix humidity_sensor.py e config file

--- PyMockSensors/src/sensor/humidity_sensor.py
+++ PyMockSensors/src/sensor/humidity_sensor.py
@@ -1,11 +1,10 @@
 from datetime import datetime
-from math import cos, pi
 from random import Random
 from typing import Type
 
 from src.sensor.sensor_interface import SensorInterface
 
-from src.toolkit.constants import SEC_IN_DAY, SEC_IN_HOUR, HUMID_RANGE
+from src.toolkit.constants import HUMID_RANGE
 from src.toolkit.coordinates import Coordinates
 from src.toolkit.sensor_type import SensorType
 
@@ -13,7 +12,7 @@ from src.toolkit.jsonfy import jsonfy
 
 
 class HumiditySensor(SensorInterface):
-    __noise = None
+    # __noise = None
 
     def __init__(self, sensor_name: str, gather_time: Type[datetime], coordinates: Coordinates, socrates: Random):
         super().__init__(sensor_name, gather_time, coordinates, socrates)
@@ -24,14 +23,14 @@ class HumiditySensor(SensorInterface):
         hour = int(now.strftime("%H"))
         month = now.month
         absolute_humidity, night_saturation, mid_saturation, noon_saturation = HUMID_RANGE[int(month)]
-        variance = round(self._socrates.random(), 2)
-        absolte_value = Random.uniform(absolute_humidity-variance, absolute_humidity+variance)
+        variance = self._socrates.random()
+        absolute_value = self._socrates.uniform(absolute_humidity - variance, absolute_humidity + variance)
         if (hour >= 5 and hour < 11) or (hour >= 17 and hour < 23):
-            simulated_value = round((absolte_value / mid_saturation) * 100)
+            simulated_value = round((absolute_value / mid_saturation) * 100)
         if hour >= 11 and hour < 17:
-            simulated_value = round((absolte_value / noon_saturation) * 100)
+            simulated_value = round((absolute_value / noon_saturation) * 100)
         if hour >= 23 or hour < 5:
-            simulated_value = round((absolte_value / night_saturation) * 100)
+            simulated_value = round((absolute_value / night_saturation) * 100)
         if simulated_value > 100: 
             simulated_value = 100
         reading = {
