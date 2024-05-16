fe3c2bc6ee4e950b74f284fa2bcbcb109435a32d
Guglielmo Barison <guglielmobarison00@gmail.com>
2024-05-16 02:47:14 +0200

    fix typo

--- PyMockSensors/test/unit/test_jsonfy_function.py
+++ PyMockSensors/test/unit/test_jsonfy_function.py
@@ -14,8 +14,7 @@ class TestJsonfy(unittest.TestCase):
         readings = [{"type": "Degrees Celsius", "value":  25.85}]
         coordinates.toJson = MagicMock(return_value='{"type": "point", "coordinates": [45.406434, 11.879008]}')
         json_output = jsonfy(sensor_name, sensor_id, sensor_type, gather_time, coordinates, readings)
-
-        expected_json = '{"sensor_name": "Arcella", "sensor_id": "mocked_id", "sensor_type": "temperature", "gather_time": "2024-04-26 12:10:00.123456", "readings": [{"type": "Degrees Celsius", "value":  25.85}], "coordinates": {"type": "point", "coordinates": [45.406434, 11.879008]}}'
+        expected_json = '{"sensor_type": "temperature", "sensor_name": "Arcella", "sensor_id": "mocked_id", "gather_time": "2024-04-26 12:10:00.123456", "readings": [{"type": "Degrees Celsius", "value": 25.85}], "coordinates": {"type": "point", "coordinates": [45.406434, 11.879008]}}'
         assert json_output == expected_json
 
 if __name__ == '__main__':
