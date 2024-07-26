61dfb9349c4f3aef037442fceceaf31211acd2f4
Guglielmo Barison <guglielmobarison00@gmail.com>
2024-07-26 22:19:59 +0200

    fix deserializer and connection to kafka source

--- flink/src/main/java/org/nan1fy/synccity/TemperatureJob.java
+++ flink/src/main/java/org/nan1fy/synccity/TemperatureJob.java
@@ -1,30 +1,34 @@
 package org.nan1fy.synccity;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.time.Time;
 import lombok.AllArgsConstructor;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import java.util.concurrent.TimeUnit;
 
 @AllArgsConstructor
 public class TemperatureJob {
     private static final String TEMPERATURE_TOPIC = "temperature";
 
     public static void main(String[] args) throws Exception {
-        var bootstrapHost = System.getenv("KAFKA_HOST");
-        var bootstrapPort = System.getenv("KAFKA_PORT");
-        var bootstrapServers = bootstrapHost + ":" + bootstrapPort;
+        var bootstrapServers = System.getenv("BOOTSTRAP_SERVERS");
         var schemaRegistryUrl = System.getenv("SCHEMA_REGISTRY_URL");
 
-        if (bootstrapHost == null || bootstrapPort == null || schemaRegistryUrl == null) {
+        if (bootstrapServers == null || schemaRegistryUrl == null) {
             throw new IllegalArgumentException("BOOTSTRAP_SERVERS and SCHEMA_REGISTRY_URL must be set");
         } else {
             System.out.println("Connecting to bootstrap server " + bootstrapServers);
         }
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(
+                5, // number of restart attempts
+                Time.of(15, TimeUnit.SECONDS) // delay
+        ));
 
         KafkaSource<TemperatureReading> temperatureSource = KafkaSource.<TemperatureReading>builder()
                 .setStartingOffsets(OffsetsInitializer.latest())
@@ -42,7 +46,9 @@ public class TemperatureJob {
                         "temperatureSource"
                 );
 
-        temperatureStream.print();
+        temperatureStream
+                .map(TemperatureReading::toString)
+                .print();
 
         env.execute("temperature_job");
 
--- flink/src/main/java/org/nan1fy/synccity/TemperatureReading.java
+++ flink/src/main/java/org/nan1fy/synccity/TemperatureReading.java
@@ -1,10 +1,51 @@
 package org.nan1fy.synccity;
-//Json even that needs to be deserialised
+
+import java.util.List;
+
 public class TemperatureReading {
-    public String sensorName;
-    public String sensorId;
-    public String gatheTime;
-    public Double latitude;
-    public Double longitude;
-    public Double temperature;
+
+    public String sensor_type;
+    public String sensor_name;
+    public String sensor_id;
+    public String gather_time;
+    public List<Reading> readings;
+    public Coordinates coordinates;
+
+    @Override
+    public String toString() {
+        return "TemperatureReading{" +
+                "sensor_type='" + sensor_type + '\'' +
+                ", sensor_name='" + sensor_name + '\'' +
+                ", sensor_id='" + sensor_id + '\'' +
+                ", gather_time='" + gather_time + '\'' +
+                ", readings=" + readings +
+                ", coordinates=" + coordinates +
+                '}';
+    }
+
+    public static class Reading {
+        public String type;
+        public Double value;
+
+        @Override
+        public String toString() {
+            return "Reading{" +
+                    "type='" + type + '\'' +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+
+    public static class Coordinates {
+        public String type;
+        public List<Double> coordinates;
+
+        @Override
+        public String toString() {
+            return "Coordinates{" +
+                    "type='" + type + '\'' +
+                    ", coordinates=" + coordinates +
+                    '}';
+        }
+    }
 }
